bc3223707334ad14a8b47441ae2cec2c902042e0
Rapidash001 <simbulanjohnmichael.10@gmail.com>
2025-05-29 23:42:54 +0800

    modified na

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Fascades\DB;
 use Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use App\Models\employee;
 
 class employeecontroller extends Controller
 {
     public function index()
     {   
-
-        return view ('employee.index');
+        $employees = employee::get();
+        return view ('employee.index',compact('employees'));
     }
 
     public function create()
@@ -23,16 +24,16 @@ class employeecontroller extends Controller
     public function store(Request $request)
     {
     $request->validate([
-        'fname' => 'required|max:255|',
-        'lname' => 'required|max:255|',
-        'midname' => 'required|max:255|',
-        'age' => 'required|',
-        'address' => 'required|max:255|',
-        'zip' => 'required|',
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
         
     ]);
 
-    ::create($request->all());
+    employee::create($request->all());
     return view ('employee.create');
     }
 
@@ -42,9 +43,9 @@ class employeecontroller extends Controller
         return view ('employee.edit');
     }
 
-    public function update(Request $request, int $id) {
+    public function (Request $request, int $id) {
         {
-            $request->validate([
+            $request->([
                 'fname' => 'required|max:255|mama ko',
                 'lname' => 'required|max:255|papa ko',
                 'midname' => 'required|max:255|ate ko',
@@ -60,8 +61,8 @@ class employeecontroller extends Controller
     }
 
     public function (int $id){
-        $employees = employee::findOrFail($id);
-        $employees->deete();
-        return redirect ()->back()->with('status','Employee Deleted');
+        $employees = employee::findOrFail();
+        $employees->();
+        return  ()->back()->with('status','Employee Deleted');
     }
 }
