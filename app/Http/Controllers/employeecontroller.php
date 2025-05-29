0352a9d69f11e4345ccf709883ac7f8f7393dbc8
Rapidash001 <simbulanjohnmichael.10@gmail.com>
2025-05-30 06:55:55 +0800

    yes

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Fascades\DB;
 use Response;
 use Illuminate\Http\Request;
-use App\Models\employees;
+use App\Models\employee;
 
 class employeecontroller extends Controller
 {
-    public function index()
+    public function index()  
     {   
-        $employee = employee::get($id);
+        $employees = employee::get();
         return view ('employee.index',compact('employees'));
     }
 
@@ -33,13 +33,13 @@ class employeecontroller extends Controller
     ]);
 
     employee::create($request->all());
-    return view ('employee.index');
+    return view ('employee.create');
     }
 
-    public function ( int $id)
+    public function edit( int $id)
     {
-        $employee = employee::find($id);
-        return view ('employee.edit');
+        $employees = employee::find($id);
+        return view ('employee.edit' , compact('employees'));
     }
 
     public function update(Request $request, int $id) {
@@ -54,14 +54,14 @@ class employeecontroller extends Controller
                 
             ]);
         
-            employee::findOrFail($id)->update();
-            return redirect ()->back()->('status','Employee Updated Successfully!');
+            employee::findOrFail($id)->update($request->all());
+            return redirect ()->back()->with('status','Employee Updated Successfully!');
             }
     }
 
     public function destroy(int $id){
         $employees = employee::findOrFail($id);
         $employees->delete();
-        return  redirect()->back()->('status','Employee Deleted');
+        return  redirect()->back()->with('status','Employee Deleted');
     }
 }
